499ddd38118c4d29c41bb642ed45c17e696730ba
ZhuChongLin <8210856@qq.com>
2024-12-16 17:36:33 +0800

    fix(router): 修复 go 方法导致的多次导航问题
    - 添加 _pendingGo 属性标记 go 方法触发的导航
    - 修改 go 方法使用 navigate 而不是 replace
    - 调整 pushHistory 方法以处理 go 导航的情况

--- lib/router/memory-router.ts
+++ lib/router/memory-router.ts
@@ -1,5 +1,5 @@
 import Router from './router.js'
-import type { RouteLocation, RouterOptions } from './type.js'
+import { type RouteLocation, type RouterOptions } from './type.js'
 
 /**
  * 基于内存实现的路由器
@@ -9,6 +9,8 @@ import type { RouteLocation, RouterOptions } from './type.js'
 export default class MemoryRouter extends Router {
   // 路由历史记录数组
   protected _history: RouteLocation[] = [this.currentRouteLocation]
+  // 标记是否有go方法触发的导航
+  private _pendingGo: RouteLocation | null = null
 
   constructor(options: RouterOptions) {
     super(options)
@@ -19,16 +21,13 @@ export default class MemoryRouter extends Router {
    */
   public override go(delta: number = 1): void {
     const targetIndex = this._history.length - 1 + (delta || 0)
-
     // 如果目标索引在有效范围内
     if (targetIndex >= 0 && targetIndex < this._history.length) {
       const target = this._history[targetIndex]
-      this.replace({
-        index: target.index,
-        hash: target.hash,
-        query: target.query,
-        params: target.params
-      }).then()
+      this._pendingGo = target
+      this.navigate(target).then(() => {
+        this._pendingGo = null
+      })
     }
   }
 
@@ -40,8 +39,12 @@ export default class MemoryRouter extends Router {
    * @private
    */
   protected override pushHistory(data: RouteLocation): void {
-    this._history.push(data)
-    this.completeNavigation()
+    if (this._pendingGo) {
+      this.completeNavigation(data)
+    } else {
+      this._history.push(data)
+      this.completeNavigation()
+    }
   }
 
   /**
