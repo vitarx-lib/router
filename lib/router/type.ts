47a7ab9f835ab23effc85517831124fa912096ec
ZhuChongLin <8210856@qq.com>
2025-01-02 22:01:12 +0800

    refactor(router): 扩展 RouteIndex 类型定义
    - 在 RouteIndex 类型中添加 VitarxRouterRouteIndexTyped 选项
    - 这个改动与路由功能的扩展或定制有关

--- lib/router/type.ts
+++ lib/router/type.ts
@@ -400,7 +400,7 @@ export type RouteName = string
 /**
  * 路由索引
  */
-export type RouteIndex = RoutePath | RouteName
+export type RouteIndex = RoutePath | RouteName | VitarxRouterRouteIndexTyped
 
 /**
  * 路由目标
