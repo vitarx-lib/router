e07768da62da60910d7ae6c47ae48b84698b1517
ZhuChongLin <8210856@qq.com>
2025-01-02 22:00:22 +0800

    build(vite): 配置 dts 插件生成入口文件声明
    - 在 vite.config.lib.ts 文件中更新 dtsPlugin 配置
    - 添加 insertTypesEntry 和 rollupTypes 选项以优化类型定义生成

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -3,7 +3,14 @@ import vitarxPlugin from 'vite-plugin-vitarx'
 import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vitarxPlugin(), dtsPlugin({ include: ['lib'] })],
+  plugins: [
+    vitarxPlugin(),
+    dtsPlugin({
+      include: ['lib'],
+      insertTypesEntry: true,
+      rollupTypes: true
+    })
+  ],
   build: {
     lib: {
       entry: 'lib/index.ts',
