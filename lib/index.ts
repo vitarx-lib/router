d24a053e1d0c66455d21f00f413915c432ed9298
ZhuChongLin <8210856@qq.com>
2025-01-02 22:01:00 +0800

    refactor(lib): 添加路由索引类型声明提升开发体验
    - 在 lib/index.ts 中添加了全局类型声明 VitarxRouterRouteIndexTyped
    - 该类型用于扩展路由索引类型，完善路由索引提示
    - 开发者可在项目全局类型声明文件中定义自定义路由类型

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,15 @@
 export * from './widget/index.js'
 export * from './router/index.js'
+
+declare global {
+  /**
+   * 此全局类型用于，扩展路由索引类型，完善路由索引提示，提升开发体验。
+   *
+   * @example
+   * ```ts
+   * // 在项目中的全局类型声明文件中写入如下类型即可在使用push,replace方法时提示路由索引
+   * type VitarxRouterRouteIndexTyped = 'home'|'page' // 添加自定义的路由
+   * ```
+   */
+  type VitarxRouterRouteIndexTyped = string
+}
