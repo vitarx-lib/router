4c44e048a8bdabf9c30cf2e50eff0c4acd56a1d4
ZhuChongLin <8210856@qq.com>
2026-05-16 18:15:58 +0800

    refactor(router): 重命名工具函数以提升语义表达
    - 将 removePathEndSlash 重命名为 removeTrailingSlash
    - 更新所有调用处以使用新函数名 removeTrailingSlash
    - 保留函数功能，确保去除路径末尾斜杠逻辑不变
    - 优化代码可读性，增强函数名的表达准确性

--- packages/router/src/core/shared/utils.ts
+++ packages/router/src/core/shared/utils.ts
@@ -49,7 +49,7 @@ export function stringifyQuery(obj: Record<string, string>): `?${string}` | '' {
  * @param {string} str - 路径字符串
  * @return {string} - 去除末尾斜杠后的路径字符串
  */
-export function removePathEndSlash<T extends string>(str: T): T {
+export function removeTrailingSlash<T extends string>(str: T): T {
   if (str === '/') return str
   return str.endsWith('/') ? (str.slice(0, -1) as T) : str
 }
@@ -74,7 +74,7 @@ export function normalizePath(path: string, removeEndSlash: boolean = false): `/
   // 去除所有空格 处理重复//
   let normalizedPath = `/${path.trim()}`.replace(/\s+/g, '').replace(/\/+/g, '/')
   if (removeEndSlash) {
-    normalizedPath = removePathEndSlash(normalizedPath)
+    normalizedPath = removeTrailingSlash(normalizedPath)
   }
   // 去除尾随斜杠
   return normalizedPath as `/${string}`
