3a94bec1852152b837de7996b27fe5df4e7f0f95
ZhuChongLin <8210856@qq.com>
2026-05-16 18:16:07 +0800

    feat(router): 导出路由相关的辅助类型判断函数
    - 从 common/utils.js 中导出 isNavTarget、isNavIndex、isRouteLocation、isRoutePath 和 isExternalLink 函数
    - 方便外部模块对路由相关类型进行类型判断和辅助处理
    - 优化了 router 包的公共接口，增强可用性与扩展性

--- packages/router/src/core/index.ts
+++ packages/router/src/core/index.ts
@@ -1,4 +1,11 @@
 export * from './common/constant.js'
+export {
+  isNavTarget,
+  isNavIndex,
+  isRouteLocation,
+  isRoutePath,
+  isExternalLink
+} from './common/utils.js'
 export * from './router/index.js'
 export * from './shared/index.js'
 export type * from './types/index.js'
